f95f804a4458fd9dffa4ca87ecac1ea2b7d33ea5
JGW-Korea <jgw6372@gmail.com>
2025-05-03 03:31:56 +0900

    MovieItem 컴포넌트

---
+++ src/components/MovieItem.js
@@ -0,0 +1,28 @@
+import { Component } from "../core";
+
+// 단일 영화 카드 컴포넌트
+export default class MovieItem extends Component {
+  constructor(props) {
+    super({
+      tagName: "a",
+      props,
+    });
+  }
+
+  render() {
+    const { movie } = this.props;
+
+    // 컴포넌트 HTML 속성 설정
+    this.el.setAttribute("href", `#/movie?id=${movie.imdbID}`);
+    this.el.classList.add("movie");
+    this.el.style.backgroundImage = `url(${movie.Poster})`;
+
+    // 컴포넌트 자식 요소 구성
+    this.el.innerHTML = /* html */ `
+      <div class="info">
+        <div class="year">${movie.Year}</div>
+        <div class="title">${movie.Title}</div>
+      </div>
+    `;
+  }
+}
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,6 @@
 import { Component } from "../core";
 import movieStore from "../store/movie";
+import MovieItem from "./MovieItem";
 
 export default class MovieList extends Component {
   constructor() {
@@ -16,10 +17,11 @@ export default class MovieList extends Component {
       <div class="movies"></div>
     `;
 
+    // 입력한 영화 제목에 알맞은 영화 요소 컴포넌트 생성
     const moviesEl = this.el.querySelector(".movies");
     moviesEl.append(
-      movieStore.state.movies.map((movie) => {
-        return movie.Title;
+      ...movieStore.state.movies.map((movie) => {
+        return new MovieItem({ movie }).el;
       })
     );
   }
