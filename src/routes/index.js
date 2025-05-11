c8bc860b9a5baeb13fcb5c1767200ba5c0f80e03
JGW-Korea <jgw6372@gmail.com>
2025-05-12 03:14:33 +0900

    404 Page Not Found

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import { createRouter } from "../core";
 import About from "./About";
 import Home from "./Home";
 import Movie from "./Movie";
+import NotFound from "./NotFound";
 
 // createRouter 함수를 통해 경로(Path)와 해당 경로에서 렌더링할 컴포넌트(라우트, Route)를 정의
 export default createRouter([
   { path: "#/", component: Home },
   { path: "#/movie", component: Movie },
   { path: "#/about", component: About },
+  { path: ".*", component: NotFound },
 ]);
