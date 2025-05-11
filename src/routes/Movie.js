e45007567f7d3e7e8d51d01501d71037ae1759d2
JGW-Korea <jgw6372@gmail.com>
2025-05-12 01:12:01 +0900

    영화 상세 정보 페이지의 스켈레톤 UI 로딩

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -4,13 +4,23 @@ import movieStore, { getMovieDetails } from "../store/movie";
 // 영화 상세 페이지 라우트 컴포넌트
 export default class Movie extends Component {
   async render() {
+    this.el.classList.add("container", "the-movie");
+
+    this.el.innerHTML = /* html */ `
+      <div class="poster skeleton"></div>
+      <div class="specs">
+        <div class="title skeleton"></div>
+        <div class="labels skeleton"></div>
+        <div class="plot skeleton"></div>
+      </div>
+    `;
+
     await getMovieDetails(history.state.id); // 쿼리 스트링으로 보내진 id 값을 기준으로 영화 상세 정보 조회
     console.log(movieStore.state.movie);
 
     const { movie } = movieStore.state;
 
     // 라우트 컴포넌트 요소 구성
-    this.el.classList.add("container", "the-movie");
     this.el.innerHTML = /* html */ `
       <div class="poster" style="background-image: url(${movie.Poster.replace("SX300", "SX700")})"></div>
       <div class="specs">
