9b91170cfae5fc22b1389a40d536f945d7b0f6a4
JGW-Korea <jgw6372@gmail.com>
2025-06-20 15:50:22 +0900

    refact: About 타입 시스템 적용

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,7 +1,16 @@
 import { Store } from "../core";
 
+interface AboutState {
+  photo: string;
+  name: string;
+  email: string;
+  blog: string;
+  github: string;
+  repository: string;
+}
+
 // 사용자 정보와 관련된 상태를 저장하는 전역 스토어
-export default new Store({
+export default new Store<AboutState>({
   photo: "https://heropy.blog/css/images/logo.png",
   name: "Vito / JoGyeWon",
   email: "jgw6372@gmail.com",
