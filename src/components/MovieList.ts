d9b454fef6e86300e0f2dd7ce51856ec292dd538
JGW-Korea <jgw6372@gmail.com>
2025-06-20 17:02:14 +0900

    refact: 남은 모든 컴포넌트 타입 시스템 적용

--- src/components/MovieList.ts
+++ src/components/MovieList.ts
@@ -43,6 +43,6 @@ export default class MovieList extends Component {
     );
 
     const loaderEl = this.el.querySelector(".spinning-loader");
-    movieStore.state.loading ? loaderEl.classList.remove("hide") : loaderEl.classList.add("hide");
+    movieStore.state.loading ? loaderEl?.classList.remove("hide") : loaderEl?.classList.add("hide");
   }
 }
